a76fdeef3354bce3838d97a7df30ffa85b1d5ac7
Pedro Pacheco <pacheckdev@gmail.com>
2021-07-17 18:38:08 -0300

    dinamic axios instance

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({
-    baseURL: "https://api.themoviedb.org/3/movie/popular?api_key=cb885258165379dc7088a315eb854018&language=en-US&"
+const API = (media) => axios.create({
+    baseURL: `https://api.themoviedb.org/3/${media}/popular?api_key=cb885258165379dc7088a315eb854018&language=en-US`
 });
 
 export default API;
