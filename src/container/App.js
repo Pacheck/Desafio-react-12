cffd16121fb79479840f66c2215a8ee97455dcc7
Pedro Pacheco <pacheckdev@gmail.com>
2021-07-17 18:36:53 -0300

    adding principle of single responsability into this component

--- src/container/App.js
+++ src/container/App.js
@@ -1,59 +1,7 @@
+import React from 'react';
 
-import React, { useState, useEffect } from 'react';
+import Routes from '../routes';
 
-import api from '../services/api';
-
-import { Container, CardsWrapper, ActionsWrapper } from './styles';
-
-import Card from '../components/Card';
-
-import Button from '../components/Button';
-
-
-const App = () => {
-  const [movies, setMovies] = useState([]);
-  const [page, setPage] = useState(1);
-  const [isBlurred, setIsBlurred] = useState(false);
-
-  const handleGetMoviesFromAPI = async() => {
-    const response = await api.get(`/page=${page}`);
-    console.log(response);
-    setMovies(response.data.results)
-  }
-
-  const handleActionPage = (actionValue) => {
-    if(page + actionValue > 1)
-      setPage(prevState => prevState + actionValue)
-  };
-
-  const handleMouseEnter = () => {
-    setIsBlurred(true);
-  }
-
-  const handleMouseLeave = () => {
-    setIsBlurred(false);
-  }
-
-  useEffect(() => {
-    handleGetMoviesFromAPI();
-  }, [page])
-
-  return (
-    <Container>
-      <CardsWrapper>
-        { 
-          movies.map(movie => (
-            <Card movieInfo={movie} key={movie.id} isBlurred={isBlurred} handleMouseEnter={handleMouseEnter} handleMouseLeave={handleMouseLeave}/>
-          )) 
-        }
-      </CardsWrapper>
-      <ActionsWrapper>
-        <Button text="Previous page" handleClick={() => handleActionPage(-1)} />
-        <Button text="Next page" handleClick={() => handleActionPage(1)} />
-        {/* Fiz o sistema de paginação pra depois descobrir que independente da página que eu faça request, sempre retorna o mesmo JSON *triste* */}
-      </ActionsWrapper>
-    </Container>
-  );
-}
+const App = () => <Routes />
 
 export default App;
--- src/container/styles.js
+++ src/container/styles.js
@@ -9,15 +9,6 @@ export const GlobalStyle = createGlobalStyle`
     }
 `;
 
-export const Container = styled.div`
-    background-color: darkgray;
-    width: 100%;
-`;
 
-export const CardsWrapper = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-evenly;
-`;
 
 export const ActionsWrapper = styled.div``;
